1149a698a1051d68f2635a7fd522ec2be367427b
Ladd1993 <aladd1993@gmail.com>
2022-08-18 22:05:47 -0500

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,48 @@
+import { BsFacebook, BsLinkedin, BsInstagram } from "react-icons/bs"
+import { AiOutlineMail } from "react-icons/ai"
+import { useState } from "react";
+const Footer = () => {
+    const [chooseEmail, setChooseEmail] = useState(false);
+    const bupPrimary = "hello@bup.bio"
+    const bupDesign = "design@bup.bio"
+    const bupEnterprise = "enterprise@bup.bio"
+
+
+
+    return (
+        <div className="pt-[150px] flex  gap-y-5 items-center pb-3 justify-center">
+            <span className="mr-10">Copyright &copy; {new Date().getFullYear()} BŪP®</span>
+            <div className="flex items-center gap-x-5">
+                <BsFacebook className="text-base" /> | <BsLinkedin className="text-base" />
+                | <BsInstagram className="text-base" /> |
+
+
+
+                <>
+                    <label for="my-modal" class="btn modal-button"><AiOutlineMail
+                        className="text-base"
+
+                    /></label>
+
+                    <input type="checkbox" id="my-modal" class="modal-toggle" />
+                    <div class="modal">
+                        <div class="modal-box">
+                            <h3 class="font-bold text-lg text-center">What would you like to ask us about?</h3>
+                            <div className="flex flex-col items-center pt-10">
+                                <a href="mailto: hello@bup.bio">hello@bup.bio</a>
+                                <a href="mailto: design@bup.bio">design@bup.bio</a>
+                                <a href="mailto: enterprise@bup.bio">enterprise@bup.bio</a>
+                            </div>
+                            <div class="modal-action">
+                                <label for="my-modal" class="btn">Close</label>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            </div>
+
+        </div>
+    )
+}
+
+export default Footer

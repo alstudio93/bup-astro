e41b343782155323e79dfe9d965428dc8a300563
Ladd1993 <aladd1993@gmail.com>
2022-08-20 03:15:11 -0500

    styling

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -1,5 +1,5 @@
 // import { Image } from "@astrojs/image/components";
-import Marquee from "react-fast-marquee";
+// import Marquee from "react-fast-marquee";
 
 const Brands = () => {
     return (

edc03871271ee0006dad25c9119c54bc533cd90e
Ladd1993 <aladd1993@gmail.com>
2022-08-19 21:28:24 -0500

    navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 
 const Navbar = () => {
   const Links = [
@@ -35,22 +36,26 @@ const Navbar = () => {
     },
   ]
 
+  const [open, setOpen] = useState(false)
+
   return (
     <div id="stylingWrapper" className="w-full shadow-md mb-5 p-4 fixed top-0 z-40 bg-white">
-      <nav className="flex justify-between w-full max-w-section mx-auto ">
+      <nav className="flex justify-between items-center w-full max-w-section mx-auto ">
         <img src="/assets/logo.webp" className="w-20" />
-        <ul className="flex gap-x-3 text-sm items-center font-[Nunito] font-normal">
+        <ul className={` lg:flex gap-x-3  text-center font-[Nunito] font-normal ${open ? "flex flex-col  gap-y-8 fixed top-14 left-0 right-0 min-h-screen bg-[#ffffff] text-base pt-20" : "hidden text-sm"}`}>
           {
             Links.map((link) => (
-              <li key={link.title}>
-                <a href={link.location}>{link.title}</a>
-              </li>
+              <div>
+                <li key={link.title} className=" pb-10">
+                  <a href={link.location}>{link.title}</a>
+                </li>
+                <hr className="w-4/5 mx-auto" />
+              </div>
             ))
           }
-
-
+          <div className="lg:hidden blob w-full" />
         </ul>
-
+        <i className="fas fa-bars cursor-pointer lg:hidden text-xl" onClick={() => setOpen(open => !open)}></i>
       </nav>
     </div>
   )

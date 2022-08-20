d756f42323ad3e9f0ce1eaed7c5d994c83089b94
Ladd1993 <aladd1993@gmail.com>
2022-08-20 04:06:20 -0500

    marquee

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,41 +1,44 @@
 import Marquee from "react-fast-marquee";
+import img1 from "/assets/brandImages/bwim-logo.webp";
 
-const Images = () => {
-    const imageArray = [
-        {
-            src: "../assets/brandImages/bwim-logo.webp",
-        },
-        {
-            src: "../assets/brandImages/century-21-be3-logo.webp",
-        },
-        {
-            src: "../assets/brandImages/century-21-be3.webp",
-        },
-        {
-            src: "../assets/brandImages/dwntwn-social.webp",
-        },
-        {
-            src: "../assets/brandImages/the-switzer-hych-group-logo.webp",
-        },
-        {
-            src: "../assets/brandImages/ut-logo.webp",
-        },
-    ]
-    return (
-        <>
-            {
-                imageArray.map((img) => (
-                    <img key={img.src} className="w-[150px] object-contain cursor-pointer" src={img.src} />
-                ))
-            }
-        </>
-    )
-}
+// const Images = () => {
+//     const imageArray = [
+//         {
+//             src: "../assets/brandImages/bwim-logo.webp",
+//         },
+//         {
+//             src: "../assets/brandImages/century-21-be3-logo.webp",
+//         },
+//         {
+//             src: "../assets/brandImages/century-21-be3.webp",
+//         },
+//         {
+//             src: "../assets/brandImages/dwntwn-social.webp",
+//         },
+//         {
+//             src: "../assets/brandImages/the-switzer-hych-group-logo.webp",
+//         },
+//         {
+//             src: "../assets/brandImages/ut-logo.webp",
+//         },
+//     ]
+//     return (
+//         <>
+//             {
+//                 imageArray.map((img) => (
+//                     <img key={img.src} className="w-[150px] object-contain cursor-pointer" src={img.src} />
+//                 ))
+//             }
+//         </>
+//     )
+// }
 
 const MarqueeComp = () => {
     return (
         <Marquee>
-            Hello
+            <div>
+                <img src={img1} />
+            </div>
         </Marquee>
     )
 }
